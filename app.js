30a972f8b5d37b4bba380f352776dced0bc2a4a8
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-03 17:25:29 +0100

    add reduce functions

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add(){
+      this.counter++;
+    },
+    reduce(){
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
