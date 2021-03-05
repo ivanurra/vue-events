91e3319a8d3644acc7e972b46d43902a0161c49a
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-05 10:03:23 +0100

    watchers

--- app.js
+++ app.js
@@ -4,17 +4,39 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
-    };
+      name: '',
+      lastName: ''
+      // fullname: ''
+    }
+  },
+  watch: {
+    counter(value) {
+      if (value > 50){
+        this.counter = 0;
+      } 
+    }
+    // name(value) {
+    //   if (value === ''){
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === ''){
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // }
   },
   computed: {
-    // value
     fullname() {
       console.log('Running again...');
-      if (this.name === ''){
+      if (this.name === '' || this.lastName === ''){
         return '';
       }
-      return this.name;
+      return this.name + ' ' + this.lastName;
     }
   },
   methods: {
